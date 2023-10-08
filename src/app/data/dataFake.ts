acc4a954ff613380c3876c4e93ab2663c0420eac
gleydvanmacedo <gleydvanmacedo@gmail.com>
2023-10-08 19:27:59 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://m.media-amazon.com/images/I/91tX0DgDb5L._AC_UF1000,1000_QL80_.jpg",
+    "title":"Ragna Crimson",
+    "description":"Caçadores de dragões: guerreiros armados com armas de prata especiais que matam as suas presas por recompensas. O mais baixo entre as suas fileiras é Ragna, que forma uma parceria improvável com a jovem génio Leonica, uma mestre matadora de dragões com mais mortes em seu nome do que quase qualquer outro. Tudo o que Ragna quer é ficar ao lado de Leonica, mas o seu sonho é destruído por um ataque do dragão mais mortal que se possa imaginar…"
+  },
+  {
+    "id":"2",
+    "photoCover":"https://www.intoxianime.com/wp-content/uploads/2021/09/e-iemgovca0wn-y.jpg",
+    "title":"Under Ninja",
+    "description":"Com o fim da 2º Guerra Mundial o Japão criou um grupo de ninjas de elite para servir como aliados e ajudar no controle da violência e terrorismo na região do pacifico. Muitos anos depois, e agora contando com mais de 20 mil agentes ativos, o programa de ninjas trabalha não só para o Japão, como também para outras nações. Kudo é um adolescente fracassado de 17 anos que tenta levar sua vida de forma tranquila, escondendo o fato de fazer parte da atual geração de ninjas da sua família, sendo então o responsável por manter a paz no Japão quando um inesperado aumento de assassinos começam a aparecer."
+  },
+  {
+    "id":"3",
+    "photoCover":"https://animefire.vip/img/animes/sousou-no-frieren-large.webp?v=1",
+    "title":"Sousou no Frieren",
+    "description":"A história acompanha a elfa maga Frieren que, após derrotar o Rei Demônio ao lado de dois humanos e um anão, trazendo paz à terra, decide refazer os passos da antiga jornada, agora sem os antigos companheiros que não gozam da mesma longevidade de uma elfa… Nesse percurso, Frieren revive os melhores momentos de sua extensa vida, numa verdadeira “busca pelo tempo perdido”"
+  },
+  {
+    "id":"4",
+    "photoCover":"https://image.tmdb.org/t/p/original/qdF4MnYEzXP0E9IrMP5IfopyOMO.jpg",
+    "title":"Hametsu no Oukoku",
+    "description":"Por eras, a humanidade floresceu através do poder da magia, um presente das bruxas para ajudar a humanidade. Mas os tempos mudaram. A expansão científica Gear tornou obsoletos tanto a magia quanto as bruxas. A fim de libertar a humanidade da praga da magia, o poderoso Império Redia iniciou uma caçada implacável para exterminar todas as bruxas. Adonis era apenas um menino quando a caçada começou, aprendiz de uma bruxa que ele amava muito. Quando ela perece nas mãos do império, Adonis jura vingança. Pelo poder deste mago furioso, o sangue fluirá!"
+  }
+]
